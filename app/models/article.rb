b5f4dd6954f0f924752ccd27218ce339914cb723
asahi <asap1017@docomo.ne.jp>
2021-06-02 18:53:48 +0900

    アイキャッチ

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,6 +17,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  has_one_attached :eyecatch
+
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_many :likes, dependent: :destroy
