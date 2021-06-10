8099ded19d9bd1f096d5f0631f4af1419a73b889
asahi <asap1017@docomo.ne.jp>
2021-06-10 11:07:02 +0900

    followの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,14 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
 
+  #following
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  #follower
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -37,6 +45,9 @@ class User < ApplicationRecord
     likes.exists?(article_id: article.id)
   end
 
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
 
   def display_name
     profile&.nickname || self.email.split('@').first
