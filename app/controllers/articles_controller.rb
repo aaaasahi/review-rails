b37d6996a9b7835cb7ddd5d1bc98fd05ef4aba26
asahi <asap1017@docomo.ne.jp>
2021-07-04 13:03:27 +0900

    保存、表示できるように

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content, :eyecatch)
+    params.require(:article).permit(:title, :content, :eyecatch, :date)
   end
 
   def set_article
--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -3,6 +3,10 @@ module ArticleDecorator
     I18n.l(self.created_at, format: :default)
   end
 
+  def display_date
+    I18n.l(self.date, format: :default)
+  end
+
   def author_name
     user.display_name
   end
